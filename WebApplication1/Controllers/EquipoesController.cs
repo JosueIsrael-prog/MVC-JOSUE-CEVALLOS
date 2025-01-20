b4d08548559dff3ae697f464530d07cfb3bc03ef
JosueIsrael-prog <131050552+JosueIsrael-prog@users.noreply.github.com>
2025-01-20 07:46:44 -0500

    Actualizar EquipoesController.cs

--- WebApplication1/Controllers/EquipoesController.cs
+++ WebApplication1/Controllers/EquipoesController.cs
@@ -1,6 +1,29 @@
-public class Country
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class CountriesViewModel
 {
-    public string Name { get; set; }
-    public string Region { get; set; }
-    public string MapsLink { get; set; }
+    public ObservableCollection<Country> Countries { get; } = new ObservableCollection<Country>();
+    HttpClient client = new HttpClient();
+
+    public async Task SearchCountry(string name)
+    {
+        var response = await client.GetAsync($"https://restcountries.com/v3.1/name/{name}?fields=name,region,maps");
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+            var countries = JsonConvert.DeserializeObject<List<Country>>(json); // Adaptar a la estructura exacta de la respuesta
+            Countries.Clear();
+            foreach (var country in countries)
+            {
+                Countries.Add(country);
+            }
+        }
+        else
+        {
+            // Manejar el error
+        }
+    }
 }
