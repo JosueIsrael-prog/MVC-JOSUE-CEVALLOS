18a69a5e10b00b481ba19b538393c79bb6686608
JosueIsrael-prog <131050552+JosueIsrael-prog@users.noreply.github.com>
2025-01-20 07:35:59 -0500

    Actualizar EquipoesController.cs

--- WebApplication1/Controllers/EquipoesController.cs
+++ WebApplication1/Controllers/EquipoesController.cs
@@ -1,12 +1,6 @@
-<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             x:Class="CevallosJosueExamenP2.RecargaTelefonicaPage"
-             Title="Recarga telefónica">
-    <StackLayout Padding="20">
-        <Label Text="Recarga telefónica" FontSize="24" HorizontalOptions="Center" />
-        <Entry x:Name="NumeroTelefono" Placeholder="Número de teléfono" />
-        <Entry x:Name="NombreUsuario" Placeholder="Nombre de quien realiza la recarga" />
-        <Button Text="Recargar" Clicked="OnRecargarClicked" />
-        <Label x:Name="UltimaRecargaLabel" Text="Última recarga realizada:" Margin="0,20,0,0" />
-    </StackLayout>
-</ContentPage>
+public class Country
+{
+    public string Name { get; set; }
+    public string Region { get; set; }
+    public string MapsLink { get; set; }
+}
