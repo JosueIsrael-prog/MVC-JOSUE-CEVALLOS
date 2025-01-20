ce552e6bb966c3f61a4cbb4b82657cb6dc6a7228
JosueIsrael-prog <131050552+JosueIsrael-prog@users.noreply.github.com>
2025-01-20 08:17:51 -0500

    Actualizar EquipoesController.cs

--- WebApplication1/Controllers/EquipoesController.cs
+++ WebApplication1/Controllers/EquipoesController.cs
@@ -1,24 +1,11 @@
-<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             x:Class="JCevallls.MainPage"
-             xmlns:vm="clr-namespace:JCevallls.ViewModels">
-    <ContentPage.BindingContext>
-        <vm:CountriesViewModel />
-    </ContentPage.BindingContext>
+using Microsoft.EntityFrameworkCore;
 
-    <VerticalStackLayout>
-        <SearchBar Placeholder="Enter country name"
-                   SearchCommand="{Binding SearchCountryCommand}"
-                   SearchCommandParameter="{Binding Text, Source={RelativeSource Self}}"/>
-        <Button Text="Search"
-                Command="{Binding SearchCountryCommand}"
-                CommandParameter="{Binding Text, Source={RelativeSource Self}}"/>
-        <ListView ItemsSource="{Binding Countries}">
-            <ListView.ItemTemplate>
-                <DataTemplate>
-                    <TextCell Text="{Binding CommonName}" Detail="{Binding Region}"/>
-                </DataTemplate>
-            </ListView.ItemTemplate>
-        </ListView>
-    </VerticalStackLayout>
-</ContentPage>
+public class AppDbContext : DbContext
+{
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Filename=MyCountriesDb.db");
+    }
+}
